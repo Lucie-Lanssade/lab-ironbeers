ab8dc5aebd94809e96ad5ea98a399d75cc3bdf2c
Lucie-Lanssade <lucie.lanssade@gmail.com>
2023-02-09 09:10:26 +0100

    lab

--- app.js
+++ app.js
@@ -13,13 +13,34 @@ app.set('views', path.join(__dirname, 'views'));
 app.use(express.static(path.join(__dirname, 'public')));
 
 // Register the location for handlebars partials here:
-
+hbs.registerPartials(path.join(__dirname, 'views', 'partials'));
 // ...
 
 // Add the route handlers here:
 
-app.get('/', (req, res) => {
-  res.render('index');
+app.get('/', (request, response) => {
+  response.render('index', { title: 'home' });
 });
 
-app.listen(3000, () => console.log('🏃‍ on port 3000'));
+app.get('/beers', async (req, res, next) => {
+  try {
+    const allBeers = await punkAPI.getBeers();
+
+    res.render('beers', { allBeers, title: 'All Beers' });
+  } catch (error) {
+    next(error);
+  }
+});
+
+app.get('/random-beer', async (request, response) => {
+  try {
+    const random = await punkAPI.getRandom();
+    response.render('random-beer', { random: random[0], title: 'Random Beer' });
+  } catch (error) {
+    next(error);
+  }
+});
+
+app.listen(3000, () => console.log('🏃‍ http://localhost:3000'));
+
+//test commit
